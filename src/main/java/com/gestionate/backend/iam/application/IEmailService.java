41e9d869c91eb96a5a1b93af92f65667a6ac492e
oneRustMan <u202421876@upc.edu.pe>
2026-06-13 01:14:18 -0500

    feat(communication): add contact auto reply email

--- src/main/java/com/gestionate/backend/iam/application/IEmailService.java
+++ src/main/java/com/gestionate/backend/iam/application/IEmailService.java
@@ -5,4 +5,6 @@ public interface IEmailService {
     void sendPasswordResetCode(String to, String code);
 
     void sendReportStatusNotification(String to, String title, String message);
+
+    void sendContactAutoReply(String to, String name, String registerUrl);
 }
